076849ef8be7d21ad0afa7425e0cd1586a4146ca
ChrisGleich1AufsMaul <49039734+ChrisGleich1AufsMaul@users.noreply.github.com>
2026-06-22 13:53:46 +0200

    Update FLEXBridge.m

--- Sources/FLEXBridge.m
+++ Sources/FLEXBridge.m
@@ -1,8 +1,31 @@
-#import <UIKit/UIKit.h>
-#import <FLEX/FLEXManager.h>
+#import "FLEXBridge.h"
 
-@interface FLEXBridge : NSObject
-+ (instancetype)sharedInstance;
-- (void)presentExplorerIfPossible;
-- (void)hideExplorerIfNeeded;
+@implementation FLEXBridge {
+    BOOL _isPresenting;
+}
+
++ (instancetype)sharedInstance {
+    static FLEXBridge *shared;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shared = [self new];
+    });
+    return shared;
+}
+
+- (void)presentExplorerIfPossible {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (self->_isPresenting) return;
+        self->_isPresenting = YES;
+        [[FLEXManager sharedManager] showExplorer];
+    });
+}
+
+- (void)hideExplorerIfNeeded {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[FLEXManager sharedManager] hideExplorer];
+        self->_isPresenting = NO;
+    });
+}
+
 @end

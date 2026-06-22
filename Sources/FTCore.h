af0e68584357962191df2b78eb712a8ed5359165
ChrisGleich1AufsMaul <49039734+ChrisGleich1AufsMaul@users.noreply.github.com>
2026-06-22 13:57:37 +0200

    Update FTCore.h

--- Sources/FTCore.h
+++ Sources/FTCore.h
@@ -1,1 +1,7 @@
+#import <Foundation/Foundation.h>
 
+@interface FTCore : NSObject
++ (instancetype)sharedInstance;
+- (void)installLifecycleMonitoring;
+- (void)scheduleExplorerPresentationIfNeeded;
+@end

7a6e3be9399f10477f8285f07d0b5040395e2c62
ChrisGleich1AufsMaul <49039734+ChrisGleich1AufsMaul@users.noreply.github.com>
2026-06-22 13:54:01 +0200

    Update FLEXBridge.h

--- Sources/FLEXBridge.h
+++ Sources/FLEXBridge.h
@@ -1,1 +1,8 @@
+#import <UIKit/UIKit.h>
+#import <FLEX/FLEXManager.h>
 
+@interface FLEXBridge : NSObject
++ (instancetype)sharedInstance;
+- (void)presentExplorerIfPossible;
+- (void)hideExplorerIfNeeded;
+@end
